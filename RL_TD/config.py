bb08c63ab0ba32e72d9cf274a7e830413ab24a71
Jaime_Predator <jgonzalez4900@alumno.uned.es>
2023-10-12 11:20:08 +0200

    [MISC] New branch

--- RL_TD/config.py
+++ RL_TD/config.py
@@ -3,7 +3,7 @@ import os
 config = {
     "Data": {
         "Loader": {
-            "folder_path": [dirpath for dirpath, dirname, _ in os.walk(os.getcwd()) if 'resources' in dirname][0]
+            "folder_path": [dir_path for dir_path, dir_name, _ in os.walk(os.getcwd()) if 'resources' in dir_name][0]
         },
     },
     "Model": {}
