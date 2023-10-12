98eda8e5c913778330aba2fb7f18b8b9dcc8e818
Jaime_Predator <jgonzalez4900@alumno.uned.es>
2023-10-12 13:09:13 +0200

    [IMG] Added BUSI Dataset

--- RL_TD/config.py
+++ RL_TD/config.py
@@ -3,7 +3,11 @@ import os
 config = {
     "Data": {
         "Loader": {
-            "folder_path": [dir_path for dir_path, dir_name, _ in os.walk(os.getcwd()) if 'resources' in dir_name][0]
+            "folder_path": [
+                os.path.join(dir_path, 'Dataset_BUSI_with_GT')
+                for dir_path, dir_name, _ in os.walk(os.getcwd())
+                if 'Dataset_BUSI_with_GT' in dir_name
+            ]
         },
     },
     "Model": {}
