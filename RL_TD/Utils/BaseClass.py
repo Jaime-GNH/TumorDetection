809b2ffd75c8b3f4377dab63f58b593551c55c4e
Jaime_Predator <jgonzalez4900@alumno.uned.es>
2023-10-13 20:13:37 +0200

    [DEV] Developing Config and Loader. Added Viewer.py. Deleted config.py and params.py

---
+++ RL_TD/Utils/BaseClass.py
@@ -0,0 +1,20 @@
+from RL_TD.Utils.DictClasses import DictClass
+
+
+class BaseClass:
+    @staticmethod
+    def _default_config(default_config, **kwargs):
+        """
+        Updates Default config with external kwargs
+        :param default_config: DefaultConfig class
+            Default config definition
+        :param kwargs: (dict)
+            kwargs
+        :return: (dict)
+            Updated kwargs
+        """
+        assert issubclass(default_config, DictClass),\
+            f'default_config must be a DefaultConfigClass subclass object.'
+        default_kwargs = default_config.to_dict()
+        default_kwargs.update(kwargs)
+        return default_kwargs
