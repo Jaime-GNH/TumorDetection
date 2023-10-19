346a0e3e1b6e5102781f8b082620f1ebe32755e7
Jaime_Predator <jgonzalez4900@alumno.uned.es>
2023-10-19 14:40:22 +0200

    [FIX] Added lightning and torchmetrics

--- TumorDetection/setup.py
+++ TumorDetection/setup.py
@@ -10,6 +10,8 @@ setup(
     install_requires=[
         'pip==23.2.1',
         'torch==2.1.0',
+        'torchmetrics==1.2.0',
+        'lightning==2.1.0',
         'opencv-python==4.8.1.78',
         'numpy==1.26.0',
         'requests',
