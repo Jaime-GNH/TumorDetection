287d16bbc5876bf1c7de6eb6d088d442ec72e4a6
Jaime González-Novo <jaimegonzaleznovohueso@gmail.com>
2023-10-24 20:30:46 +0200

    [DEV] First try edge_index with dilations. Need revision.

--- TumorDetection/Utils/Utils.py
+++ TumorDetection/Utils/Utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 from torch_geometric.data import Data
+import torch_geometric as tg
 
 
 def apply_function2list(lst, fun, **kwargs):
@@ -19,12 +20,14 @@ def apply_function2list(lst, fun, **kwargs):
     return list(map(lambda x: fun(x, **kwargs), lst))
 
 
-def tup2graph(tup, img_idx, mask_idx=None, device='cpu'):
+def tup2graph(tup, img_idx, mask_idx=None, dilations=(1, 3, 5, 7), device='cpu'):
     """
     Converts a tuple with an image to a graph.
     :param tup: tuple
     :param img_idx:
     :param mask_idx:
+    :param dilations: (tup(int))
+        Number of dilations.
     :param device:
     :return: graph
     """
@@ -38,18 +41,23 @@ def tup2graph(tup, img_idx, mask_idx=None, device='cpu'):
         y = None
 
     pos = torch.as_tensor([[i, j] for i in range(image.shape[0]) for j in range(image.shape[1])])
-    # TODO: Edge_index as [2, num_edges]
-    #  [0, edge] -> src
-    #  [1, dst] -> dst
-    #  Not possible if all connected (
-    #       src == num_nodes**2 and dst == num_nodes**2
-    #  )
-    #  Check by neighbouring and dilations
-
     graph = Data(
         x=x,
         y=y,
+        edge_index=torch.empty(),
         pos=pos
     ).to(device=device)
+
+    for d in dilations:
+        (row, col) = tg.utils.grid(3 + d, 3 + d)
+        graph.edge_index = torch.cat(
+            tensors=(graph.edge_index,
+                     torch.stack([row[::d], col[::d]])),
+            dim=1
+        )
+        graph.remove_self_loops()
+        graph.to_undirected()
+        graph.coalesce()
+
     graph.info = [l for i, l in enumerate(tup) if i not in [img_idx, mask_idx]]
     return graph
