72f0e931c13c9b2e3c3fa974e422d5121ad2e645
Jaime González-Novo <jaimegonzaleznovohueso@gmail.com>
2023-11-04 14:32:19 +0100

    [DEV] Added functionalities and improved efficiency in edge_index calculations.

--- TumorDetection/Utils/Utils.py
+++ TumorDetection/Utils/Utils.py
@@ -19,79 +19,170 @@ def apply_function2list(lst, fun, **kwargs):
     return list(map(lambda x: fun(x, **kwargs), lst))
 
 
-def tup2graph(tup, img_idx, mask_idx=None, dilations=(1, 2, 6), device='cpu'):
+def tup2graph(tup, img_idx, mask_idx=None,
+              auto_dilations=False, num_hops=None,
+              dilations=(1, 2, 6), kernel_kind='star',
+              device='cpu'):
     """
     Converts a tuple with an image to a graph.
-    :param tup: tuple
-    :param img_idx:
-    :param mask_idx:
-    :param dilations: (tup(int))
-        Number of dilations.
-    :param device:
-    :return: graph
+    :param tup: (tuple)
+        Tuple containing image, mask and additional information for graph
+    :param img_idx: (int)
+        Index for image to be transformed in tuple
+    :param mask_idx: (int, None)
+        Index of target mask in tuple
+    :param auto_dilations: (bool, False)
+        Calculate dilations automatically
+    :param num_hops: (int, None)
+        Number of message passings. Only if auto_dilations
+    :param dilations: ((tup(int), int), (1, 2, 6))
+        Number of dilations. Only if not auto_dilations
+    :param kernel_kind: (str, 'star')
+        Kernel type
+    :param device: (str, 'cpu')
+        torch device
+    :return: (torch.data.Data)
+        Homogeneous graph.
     """
-    assert all([d > 0 for d in dilations]), f'dilations must be all grater than 0. Got {dilations}'
     image = tup[img_idx]
     if len(image.shape) < 3:
         image = np.expand_dims(image, -1)
-    x = torch.as_tensor(np.vstack(image), dtype=torch.uint8)
+    if image.dtype == 'uint8' or np.max(image) > 1:
+        image = image.astype('float')/255.
+    x = torch.as_tensor(np.vstack(image), dtype=torch.float32)
     if mask_idx is not None:
-        y = torch.as_tensor(np.vstack(tup[mask_idx]), dtype=torch.uint8)
+        y = torch.as_tensor(tup[mask_idx], dtype=torch.int8).flatten().unsqueeze(dim=1).to(device)
     else:
         y = None
 
     pos = torch.as_tensor([[i, j] for i in range(image.shape[0]) for j in range(image.shape[1])],
-                          dtype=torch.int16)
+                          dtype=torch.int16).to(device)
+    pos_idx = torch.as_tensor(list(range(pos.size(dim=0))), dtype=torch.int32).to(device)
+    if not auto_dilations:
+        if isinstance(dilations, int):
+            dilations = (dilations,)
+        assert all([d > 0 for d in dilations]), f'dilations must be all grater than 0. Got {dilations}'
+    else:
+        assert num_hops is not None and num_hops > 0,\
+            f'If auto_dilations is True you must pass a positive num_hops. Got {num_hops}'
+        dilations = calculate_dilations(image_shape=image.shape, num_hops=num_hops, kernel_kind=kernel_kind)
+
+    edge_index = None
+    for d in dilations:
+        kernel = build_edge_kernel(d, kernel_kind, device)
+        for k in kernel:
+            edge_index = apply_kernel(edge_index, k, pos, pos_idx, device)
+    edge_index = tg.utils.to_undirected(edge_index=edge_index)
+    edge_index, _ = tg.utils.remove_self_loops(edge_index=edge_index)
+
     graph = Data(
         x=x,
         y=y,
-        edge_index=None,
+        edge_index=edge_index,
         pos=pos
-    ).to(device=device)
+    )
 
-    for d in dilations:
-        dst = [torch.where(
-            ((pos[:, 0] == pos[i, 0]) & (abs(pos[:, 1] - pos[i, 1]) == d)) |
-            ((abs(pos[:, 0] - pos[i, 0]) == d) & (pos[:, 1] == pos[i, 1])) |
-            ((abs(pos[:, 0] - pos[i, 0]) == d) & (abs(pos[:, 1] - pos[i, 1]) == d))
-        )[0]
-                 for i in range(graph.num_nodes)]
-
-        srcdst = [[torch.tensor([i] * len(dst_i)), dst_i] for i, dst_i in enumerate(dst)]
-        edge_index = torch.cat([torch.stack([src, dst]) for src, dst in srcdst], dim=1)
-        # src = torch.tensor([i for i in range(graph.num_nodes)]).flatten()
-        # for src_i in src:
-        #     dst_i = torch.where(
-        #         ((pos[:, 0] == pos[src_i, 0]) & (abs(pos[:, 1] - pos[src_i, 1]) == d)) |
-        #         ((abs(pos[:, 0] - pos[src_i, 0]) == d) & (pos[:, 1] == pos[src_i, 1])) |
-        #         ((abs(pos[:, 0] - pos[src_i, 0]) == d) & (abs(pos[:, 1] - pos[src_i, 1]) == d))
-        #     )[0]
-        #     edge_index_i = torch.stack([
-        #         torch.tensor([src_i] * len(dst_i)).flatten(),
-        #         dst_i
-        #     ])
-        #     graph.edge_index = (
-        #         edge_index_i if graph.edge_index is None else
-        #         torch.cat(tensors=[graph.edge_index, edge_index_i],
-        #                   dim=1))
-        graph.edge_index = (
-            edge_index if graph.edge_index is None else
-            torch.cat(tensors=[graph.edge_index, edge_index],
-                      dim=1))
-
-    graph.edge_index = tg.utils.to_undirected(edge_index=graph.edge_index)
-    graph.edge_index, _ = tg.utils.remove_self_loops(edge_index=graph.edge_index)
     graph.coalesce()
 
     graph.info = [l for i, l in enumerate(tup) if i not in [img_idx, mask_idx]]
+    graph.original_shape = image.shape
+    graph.to(device=device, non_blocking=True)
     return graph
 
-##
-# dst_i = [torch.where(
-#     ((pos[:, 0] == pos[i, 0]) & (abs(pos[:, 1] - pos[i, 1]) == d)) |
-#     ((abs(pos[:, 0] - pos[i, 0]) == d) & (pos[:, 1] == pos[i, 1])) |
-#     ((abs(pos[:, 0] - pos[i, 0]) == d) & (abs(pos[:, 1] - pos[i, 1]) == d))
-# )[0]
-# for i in range(num_nodes)]
-# srcdst=[[torch.tensor([i]*len(dst)), dst] for i, dst in enumerate(dst_i)]
-# edge_index = torch.cat([torch.stack([a,b]) for a,b in srcdst[:10]])
+
+def build_edge_kernel(d, kind='star', device='cpu'):
+    """
+    Returns a pixel to pixel kernel edge
+    :param d: (int)
+        dilation
+    :param kind: (str, 'star')
+        kernel kind
+    :param device: (str, 'cpu')
+        device
+    :return: (torch.tensor)
+        Kernel
+    """
+    if kind == 'corner':
+        return torch.as_tensor([[d, 0], [0, d]]).to(device)
+    elif kind == 'hex':
+        return torch.as_tensor([[-d, d], [d, d]]).to(device)
+    elif kind == 'star':
+        return torch.cat([build_edge_kernel(d, kind='corner', device=device),
+                          build_edge_kernel(d, kind='hex', device=device)])
+    elif kind == 'square':
+        kernel = torch.as_tensor(sum([[[i, j], [-i, j], [i, -j], [-i, -j],
+                                       [i, i], [i, -i], [-i, i], [-i, -i]] for i, j in
+                                      zip(range(d + 1), range(d, -1, -1))], [])).unique(dim=0).to(device)
+        return torch.cat([kernel[:kernel.size(dim=0)//2], kernel[kernel.size(dim=0)//2:]])
+    else:
+        raise ValueError(f'value {kind} for kernel kind is not valid.')
+
+
+def apply_kernel(edge_index, kernel, pos, pos_idx, device):
+    """
+    Apply a given kernel over a graph
+    :param edge_index: (torch.tensor)
+        Previous edge_index
+    :param kernel: (torch.tensor)
+        Kernel to apply
+    :param pos: (torch.tensor)
+        Positions of nodes
+    :param pos_idx: (torch.tensor)
+        Position index
+    :param device: (str)
+        Device to use
+    :return: (torch.tensor)
+        New egde_index
+    """
+    new_pos = pos + kernel
+    _, idx, counts = torch.cat([pos, new_pos], dim=0).unique(
+        dim=0, return_inverse=True, return_counts=True)
+    mask = torch.isin(idx, torch.where(counts.gt(1))[0])
+    edge_index_new = torch.stack([
+        pos_idx[mask[:len(pos)]],
+        pos_idx[mask[len(pos):]]
+    ]).to(device)
+    return edge_index_new if edge_index is None else torch.cat(
+        tensors=[edge_index, edge_index_new], dim=1)
+
+
+def calculate_dilations(image_shape, num_hops, kernel_kind):
+    """
+    Given an image_shape and a desired number of hops it calculates the necessary dilations to fill the space.
+    This method ensures percolation.
+    Applying this method to all images for training could incurr in loosing meaning.
+    This is a method to study the global dilations to apply.
+    :param image_shape: (tuple(int))
+        Image original shape
+    :param num_hops: (int)
+        Number of message passing
+    :param kernel_kind: (str)
+        Kernel type to calculate
+    :return: tup(int)
+        Dilations with len between 1 an 3
+    """
+    dilations = [1]
+    dy, dx = int((image_shape[0])/num_hops), int((image_shape[1])/num_hops)
+    diag = min(dy, dx)
+    offset = max(dy, dx) - diag
+    if kernel_kind == 'corner':
+        dilations += [2*diag, offset]
+    elif kernel_kind == 'hex':
+        dilations += [diag, 2*offset]
+    else:
+        dilations += [diag, offset]
+    return tuple(sorted(set([d for d in dilations if d > 0])))
+
+
+def graph_to_image_mask(graph):
+    """
+    Given a graph it returns a gray_scale image
+    :param graph:
+    :return:
+    """
+    x = graph.x[:, -1].unflatten(graph.original_shape[:2]).cpu().numpy()
+    y = graph.y
+    if y is not None:
+        y = y.unflatten(graph.original_shape[:2]).cpu().numpy()
+
+    return x, y
