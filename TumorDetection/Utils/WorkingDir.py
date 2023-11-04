b52100eae69b0c4633e58fa02c1753287a4d6fa0
Jaime González-Novo <jaimegonzaleznovohueso@gmail.com>
2023-11-04 14:33:08 +0100

    [DEV] Created WorkingDir to set a common path reference.

---
+++ TumorDetection/Utils/WorkingDir.py
@@ -0,0 +1,35 @@
+import os
+
+
+class WorkingDir:
+    default_path = '\\'.join(
+        os.path.abspath(os.getcwd()).split('\\')[
+            :os.path.abspath(os.getcwd()).split('\\').index('TumorDetection') + 1
+        ]
+    )
+
+    @classmethod
+    def set_wd(cls):
+        """
+        Sets the working directory at the top of the project.
+        :return:
+        """
+        os.chdir(
+            cls.default_path
+        )
+
+    @classmethod
+    def getwd_from_path(cls, path):
+        """
+
+        :return:
+        """
+        return os.path.abspath(
+            os.path.join(os.getcwd(),
+                         os.path.relpath(cls.default_path, start=path)
+                         )
+        )
+
+
+if __name__ == '__main__':
+    print(WorkingDir.getwd_from_path(os.getcwd()))
