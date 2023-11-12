70057ac9af5a1cfa4e3b0345b6469eaa926248f9
Jaime González-Novo <jaimegonzaleznovohueso@gmail.com>
2023-11-12 20:44:45 +0100

    [DEV] New configs

--- TumorDetection/Utils/DictClasses.py
+++ TumorDetection/Utils/DictClasses.py
@@ -2,7 +2,6 @@ import os
 import cv2
 import torch
 from datetime import datetime
-import multiprocessing as mp
 import torch_geometric as tg
 from torchmetrics.functional import accuracy, jaccard_index
 
@@ -64,40 +63,33 @@ class Device(DictClass):
 
 
 class DataPathDir(DictClass):
+    cw = WorkingDir.set_wd()
     dir_path = [
         os.path.join(dir_path, 'Dataset_BUSI_with_GT')
         for dir_path, dir_name, _ in os.walk(WorkingDir.getwd_from_path(os.getcwd()))
         if 'Dataset_BUSI_with_GT' in dir_name
     ][0]
+    os.chdir(cw)
 
 
 class ResourcesPathDir(DictClass):
+    cw = WorkingDir.set_wd()
     dir_path = [
         os.path.join(dir_path, 'resources')
         for dir_path, dir_name, _ in os.walk(WorkingDir.getwd_from_path(os.getcwd()))
         if 'resources' in dir_name
     ][0]
+    os.chdir(cw)
 
 
 class ReportingPathDir(DictClass):
+    cw = WorkingDir.set_wd()
     dir_path = [
         os.path.join(dir_path, 'reporting')
         for dir_path, dir_name, _ in os.walk(WorkingDir.getwd_from_path(os.getcwd()))
         if 'reporting' in dir_name
     ][0]
-
-
-class PicklePathDir(DictClass):
-    dir_path = [
-        os.path.join(dir_path, 'pickled_objects')
-        for dir_path, dir_name, _ in os.walk(WorkingDir.getwd_from_path(os.getcwd()))
-        if 'pickled_objects' in dir_name
-    ][0]
-
-
-class Files(DictClass):
-    pickle_loader = 'BUSI_Graphs_stacked_3classes_780_1-29-227.pkl'
-    # BUSI_Graphs_{preproceso_mode}_{num_classes}classes_{len}.pkl
+    os.chdir(cw)
 
 
 class ViewerClsParams(DictClass):
@@ -117,17 +109,19 @@ class ReadingModes(DictClass):
 class EarlyStoppingParams(DictClass):
     monitor = 'val_loss'
     mode = 'min'
-    patience = 7
+    patience = 15
     min_delta = 1e-5
+    verbose = Verbosity.get('verbose') > 0
 
 
 class ReduceLROnPLateauParams(DictClass):
     monitor = 'val_loss'
+    frequency = 1
     mode = 'min'
     patience = 3
-    min_delta = 1e-4
+    threshold = 1e-4
     factor = 1e-1
-    verbose = Verbosity.get('verbose') > 1
+    verbose = Verbosity.get('verbose') > 0
     min_lr = 1e-7
 
 
@@ -139,12 +133,30 @@ class OptimizerParams(DictClass):
     amsgrad = False
 
 
+class LightningTrainerParms(DictClass):
+    accelerator = 'auto'
+    strategy = 'auto'
+    devices = -1
+    max_epochs = 1000
+    num_sanity_val_steps = 0
+    precision = '16-mixed'
+    log_every_n_steps = 1
+    accumulate_grad_batches = 16
+    enable_progress_bar = True
+    enable_model_summary = False
+    gradient_clip_val = None
+    gradient_clip_algorithm = 'norm'
+    deterministic = False
+
+
 class ModelCkptDir(DictClass):
+    cw = WorkingDir.set_wd()
     ckpt_dir = [
-        os.path.join(dir_path, 'reporting')
+        os.path.join(dir_path, 'reporting/ckpt')
         for dir_path, dir_name, _ in os.walk(os.getcwd())
         if 'reporting' in dir_name
-    ]
+    ][0]
+    os.chdir(cw)
 
 
 # [DEFAULT CONFIGS]
@@ -169,43 +181,75 @@ class PreprocessorCall(DictClass):
     img_thresholds_std = [0, 1/4, 1/2, 1]
     clip_hist_percent = 25
     clip_limit = 5.0
-    mode = 'stacked'
+    mode = 'stacked'  # 'last'
 
 
 class ImageToGraphCall(DictClass):
     images_tup_idx = 2
     mask_tup_idx = 3
-    dilations = (1, 29, 227)
+    dilations = 1
     mask = True
-    kernel_kind = 'hex'
+    hypergraph = True
+    hypernode_patch_div = 5
+    kernel_kind = 'corner'
     device = Device.get('device')
 
 
 class GraphDatasetInit(DictClass):
-    train = Train.get('train')
     train_test_split = 0.129  # 780 / 100 -> 100 test samples.
-    inference = False
     datapathloader_transforms = DataPathLoaderCall.to_dict()
     imageloader_transforms = ImageLoaderCall.to_dict()
     preprocessor_transforms = PreprocessorCall.to_dict()
     image2graph_transforms = ImageToGraphCall.to_dict()
+    patch_dim = None
+    seed = 1234567890
 
 
-class GraphDataLoaderCall(DictClass):
+class GraphDataLoaderInit(DictClass):
+    graph_dataset_kwargs = GraphDatasetInit.to_dict()
     batch_size = 16
     num_workers = 0
-    shuffle = True
     drop_last = True
 
 
-class HyperGNNInit(DictClass):
-    h_size = [32, 64, 128]
-    layer_type = tg.nn.GATConv
+class NeighborGraphDataLoaderInit(DictClass):
+    num_nodes = int(1e4)
+    subgraph_type = 'subgraph'
+
+
+class ConvLayerKwargs(DictClass):
+    dropout = 0.2
+    act = 'gelu'
+    act_first = False
+    act_kwargs = None
+    norm = None
+    norm_kwargs = None
+    num_layers = 1
+    jk = 'max'
+
+
+class HyperConvLayerKwargs(DictClass):
+    dropout = 0.2
+    use_attention = False
+    attention_mode = 'node'
+    heads = 4
+    concat = True
+    negative_slope = 0.2
+    bias = True
+
+
+class BaseGNNInit(DictClass):
+    num_classes = len(ClassValues.to_dict()) if DataPathLoaderCall.get('map_classes') is None\
+        else len(set(DataPathLoaderCall.get('map_classes').values()))
+    h_size = [16, 32, 64, 128, 16]
+    conv_layer_type = tg.nn.GraphSAGE
+    conv_layer_kwargs = ConvLayerKwargs.to_dict()
 
 
 class LightningModelInit(DictClass):
-    model_name = 'model_' + datetime.now().strftime('%d%m%Y_%H%M')
-    criterion = torch.nn.functional.cross_entropy
+    model_name = 'model_' + 'GraphSAGE-d(1)-b(16)-patch(128)-jk-cw'  # + datetime.now().strftime('%d%m%Y_%H%M')
+    description = 'SAGE Conv con dilataciones (1,32) batch de 16, patch_dim de 128, jumpingknowledge y classweight'
+    criterion = torch.nn.CrossEntropyLoss(reduction='mean')
     metrics = {
         'accuracy': accuracy,
         'jaccard': jaccard_index
@@ -213,29 +257,23 @@ class LightningModelInit(DictClass):
     optimizer = torch.optim.Adam
     save_hyperparameters = True
     resume_training = False
-    use_earlystopping = True
-    use_modelcheckpoint = True
     use_reducelronplateau = True
-    es_kwargs = EarlyStoppingParams.to_dict()
     rlr_kwargs = ReduceLROnPLateauParams.to_dict()
-    model_kwargs = HyperGNNInit.to_dict()
+    gnn_kwargs = BaseGNNInit.to_dict()
 
 
 class TrainerInit(DictClass):
-    accelerator = 'auto'
-    strategy = 'auto'
-    devices = -1
-    precision = '32-true'
+    use_earlystopping = True
+    use_modelcheckpoint = True
+    es_kwargs = EarlyStoppingParams.to_dict()
     logger = True
-    max_epochs = 1000
-    val_check_interval = 0.
-    check_val_every_n_epoch = 1
-    enable_progress_bar = True
-    enable_model_summary = False
-    accumulate_grad_batches = 1
-    gradient_clip_val = None
-    gradient_clip_algorithm = 'norm'
-    deterministic = False
+    lightning_trainer_params = LightningTrainerParms.to_dict()
+    model_name = LightningModelInit.get('model_name')
+
+
+class TrainerCall(DictClass):
+    model_kwargs = LightningModelInit.to_dict()
+    resume_training = False
 
 
 class BaseUpdateLayout(DictClass):
