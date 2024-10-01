1047fa77f3d92b80d44f5f89d0bbea8fce90fd14
Jaime González-Novo <jaimegonzaleznovohueso@gmail.com>
2024-10-01 17:10:13 +0200

    [DOC] Final documents.

--- scripts/train_efsnet_advss.py
+++ scripts/train_efsnet_advss.py
@@ -19,12 +19,12 @@ from TumorDetection.models.adv_semi_seg.discriminator import FCDiscriminator
 DEVICE = Device.device
 TEST_SIZE = 100
 
-MODEL_NAME = 'EFSNet_ADVSS'
+MODEL_NAME = 'EFSNet_ADVSS_v1'
 INPUT_SIZE = (256, 256)
 NUM_CLASSES = 3
 CLASS_WEIGHT = torch.as_tensor([1., 5., 5.],
                                device=DEVICE)
-BATCH_SIZE = 16
+BATCH_SIZE = 8
 ITER_SIZE = 1
 LEARNING_RATE = 2.5e-4
 MOMENTUM = 0.9
@@ -37,9 +37,9 @@ WEIGHT_DECAY = 0.0005
 LEARNING_RATE_D = 1e-4
 LAMBDA_ADV_PRED = 0.1
 
-PARTIAL_DATA = 0.75
+PARTIAL_DATA = 7/8
 
-SEMI_START = 2500
+SEMI_START = 5000
 LAMBDA_SEMI = 0.1
 MASK_T = 0.2
 
@@ -93,8 +93,10 @@ def make_d_label(label, ignore_mask):
     ignore_mask = np.expand_dims(ignore_mask, axis=1)
     d_label = np.ones(ignore_mask.shape) * label
     d_label[ignore_mask] = 255
+    d_mask = (d_label >= 0) * (d_label != 255)
+    d_label = d_label[d_mask]
     d_label = Variable(torch.FloatTensor(d_label).to(DEVICE))
-    return d_label
+    return d_label, d_mask
 
 
 def main():
@@ -103,7 +105,7 @@ def main():
 
     cudnn.enabled = True
     gpu = 0
-
+    losses_gen = float('inf')
     # create network
     model = EFSNetSeg(input_shape=(1, h, w),
                       device=DEVICE)
@@ -176,6 +178,7 @@ def main():
     pred_label = 0
     gt_label = 1
     init_time = perf_counter()
+    min_loss = float('inf')
 
     for i_iter in range(NUM_STEPS):
 
@@ -215,10 +218,11 @@ def main():
                 d_out_sigmoid = torch_fun.sigmoid(d_out).data.cpu().numpy().squeeze(axis=1)
 
                 ignore_mask_remain = np.zeros(d_out_sigmoid.shape).astype(bool)
+                d_label, d_mask = make_d_label(gt_label,
+                                               ignore_mask_remain)
                 # noinspection PyUnresolvedReferences
-                loss_semi_adv = LAMBDA_SEMI_ADV * bce_loss(d_out,
-                                                           make_d_label(gt_label,
-                                                                        ignore_mask_remain).to(DEVICE))
+                loss_semi_adv = LAMBDA_SEMI_ADV * bce_loss(d_out.flatten()[d_mask.flatten()],
+                                                           d_label)
                 loss_semi_adv = loss_semi_adv / ITER_SIZE
                 loss_semi_adv_value += loss_semi_adv.data.cpu().numpy() / LAMBDA_SEMI_ADV
 
@@ -266,8 +270,9 @@ def main():
             loss_seg = loss_calc(pred, labels)
 
             d_out = interp(discr(torch_fun.softmax(pred, dim=1)))
-
-            loss_adv_pred = bce_loss(d_out, make_d_label(gt_label, ignore_mask))
+            d_label, d_mask = make_d_label(gt_label, ignore_mask)
+            loss_adv_pred = bce_loss(d_out.flatten()[d_mask.flatten()],
+                                     d_label)
 
             loss = loss_seg + LAMBDA_ADV_PRED * loss_adv_pred
 
@@ -286,14 +291,15 @@ def main():
             # train with pred
             pred = pred.detach()
 
-            if D_REMAIN:
+            if D_REMAIN and i_iter > SEMI_START_ADV:
                 # noinspection PyUnboundLocalVariable
                 pred = torch.cat((pred, pred_remain), 0)
                 # noinspection PyUnboundLocalVariable
                 ignore_mask = np.concatenate((ignore_mask, ignore_mask_remain), axis=0)
 
             d_out = interp(discr(torch_fun.softmax(pred, dim=1)))
-            loss_d = bce_loss(d_out, make_d_label(pred_label, ignore_mask))
+            d_label, d_mask = make_d_label(pred_label, ignore_mask)
+            loss_d = bce_loss(d_out.flatten()[d_mask.flatten()], d_label)
             loss_d = loss_d / ITER_SIZE / 2
             loss_d.backward()
             loss_d_value += loss_d.data.cpu().numpy()
@@ -312,26 +318,29 @@ def main():
 
             d_out = interp(discr(d_gt_v))
             # noinspection PyUnresolvedReferences
-            loss_d = bce_loss(d_out, make_d_label(gt_label, ignore_mask_gt))
+            d_label, d_mask = make_d_label(gt_label, ignore_mask_gt)
+            loss_d = bce_loss(d_out.flatten()[d_mask.flatten()], d_label)
             loss_d = loss_d / ITER_SIZE / 2
             loss_d.backward()
             loss_d_value += loss_d.data.cpu().numpy()
 
         optimizer.step()
         optimizer_d.step()
+        losses_gen = loss_seg_value + LAMBDA_ADV_PRED * loss_adv_pred_value + LAMBDA_SEMI * loss_semi_value
         et = perf_counter() - init_time
         print(
-            'iter = {0:8d}/{1:8d},'
+            'iter = {0:5d}/{1:5d},'
             ' loss_seg = {2:.3f},'
             ' loss_adv_p = {3:.3f}, '
             'loss_D = {4:.3f}, '
             'loss_semi = {5:.3f}, '
             'loss_semi_adv = {6:.3f}, '
-            'elapsed_time = {7:.3f}, '
-            'mean_iter_time = {8:.3f}'.format(
+            'min_loss = {7:.3f}, '
+            'elapsed_time = {8:.3f}, '
+            'mean_iter_time = {9:.3f}'.format(
                 i_iter, NUM_STEPS, loss_seg_value, loss_adv_pred_value,
                 loss_d_value, loss_semi_value,
-                loss_semi_adv_value, et, et/(i_iter + 1)))
+                loss_semi_adv_value, min_loss, et, et/(i_iter + 1)))
 
         if i_iter % SAVE_EVERY_NSTEPS == 0 and i_iter > 0:
             print('save model ...')
@@ -348,6 +357,17 @@ def main():
             torch.save(discr.state_dict(), os.path.join(ReportingPathDir.dir_path, 'adv_ss',
                                                         MODEL_NAME + '_discr.pt'))
             break
+        if i_iter == SEMI_START:
+            min_loss = float('inf')
+        if losses_gen < min_loss:
+            min_loss = losses_gen
+        if i_iter >= SEMI_START and losses_gen == min_loss:
+            min_loss = losses_gen
+            print('save best model ...')
+            torch.save(model.state_dict(), os.path.join(ReportingPathDir.dir_path, 'adv_ss',
+                                                        MODEL_NAME + '_best.pt'))
+            torch.save(discr.state_dict(), os.path.join(ReportingPathDir.dir_path, 'adv_ss',
+                                                        MODEL_NAME + '_discr_best.pt'))
 
 
 if __name__ == '__main__':
