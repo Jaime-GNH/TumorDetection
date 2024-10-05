0f7e6fe473db8afa3213d186dc03bf31837a04e3
Jaime González-Novo <jaimegonzaleznovohueso@gmail.com>
2024-10-05 18:50:45 +0200

    [DOC] Deleted multistage model.

--- scripts/torch_efsnet.py
+++ scripts/torch_efsnet.py
@@ -3,28 +3,10 @@ import torch
 from torchinfo import summary
 
 from TumorDetection.utils.dict_classes import Device
-from TumorDetection.models.efsnet import EFSNetClfSeg, EFSNetSeg
-
-# Classifier - Segmenter
-# model = EFSNetClfSeg()
-# summary(model, (1, 256, 256), batch_dim=0,
-#         col_names=("input_size", "output_size", "num_params", "params_percent"),
-#         depth=3,
-#         row_settings=["var_names"],
-#         device=Device.get('device'),
-#         verbose=1)
-# print(f'Device: {model.device}')
-# seg, lab = model(torch.rand((1, 1, 256, 256)).to(device=Device.get('device')))
-# images = 1000
-# it = perf_counter()
-# for _ in range(images):
-#     model(torch.rand((1, 1, 256, 256)).to(device=Device.get('device')))
-# et = perf_counter()
-# print(f'Time per 100 images: {et-it:.3f}')
-# print(f'FPS: {images/(et-it):.3f}')
+from TumorDetection.models.efsnet import EFSNet
 
 # Segmenter
-model = EFSNetSeg(input_shape=(3, 1024, 512))
+model = EFSNet(input_shape=(3, 1024, 512))
 summary(model, (3, 1024, 512), batch_dim=0,
         col_names=("input_size", "output_size", "num_params", "params_percent"),
         depth=2,
