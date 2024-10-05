394a8dcc2d508618f570b56cc79fa0d24e0e10cc
Jaime González-Novo <jaimegonzaleznovohueso@gmail.com>
2024-10-05 18:52:48 +0200

    [DEV] Added first version of K-Fold training with train, test, validation sets.

---
+++ scripts/train_kfold_efsnet.py
@@ -0,0 +1,67 @@
+import os
+from typing import Any
+from sklearn.model_selection import train_test_split
+
+from TumorDetection.data.loader import DataPathLoader
+from TumorDetection.data.dataset import TorchDataset
+from TumorDetection.utils.dict_classes import DataPathDir, ReportingPathDir, Verbosity, Device
+from TumorDetection.models.efsnet import EFSNet
+from TumorDetection.models.utils.lightning_model import LightningModel
+from TumorDetection.models.utils.trainer import Trainer
+
+MODEL_NAME: str = 'EFSNet_base'
+DESCRIPTION: str = 'EFSNet base'
+VERBOSE: int = Verbosity.get('verbose')
+DEVICE: Any = Device.get('device')
+EPOCHS: int = 2500
+IGNORE_INDEX: int = 0
+BATCH_SIZE: int = 64
+VAL_SIZE: int = 50
+TEST_SIZE: int = 50
+KFOLD: int = 5
+
+# GET DATA
+dp = DataPathLoader(DataPathDir.get('dir_path'))
+paths = dp()
+for k in range(KFOLD):
+    if not os.path.exists(os.path.join(ReportingPathDir.get('dir_path'), 'ckpt', MODEL_NAME + f'_k{k}.pt')):
+        tr_paths, val_paths = train_test_split(paths, test_size=TEST_SIZE + VAL_SIZE,
+                                               random_state=k, shuffle=True)
+        val_paths, test_paths = train_test_split(val_paths, test_size=TEST_SIZE,
+                                                 random_state=k, shuffle=True)
+
+        tr_td = TorchDataset(tr_paths,
+                             crop_prob=0.5,
+                             rotation_degrees=30,
+                             range_contrast=(0.75, 1.25),
+                             range_brightness=(0.75, 1.25),
+                             vertical_flip_prob=0.,
+                             horizontal_flip_prob=0.25)
+        val_td = TorchDataset(val_paths,
+                              crop_prob=None,
+                              rotation_degrees=None,
+                              range_contrast=None,
+                              range_brightness=None,
+                              vertical_flip_prob=None,
+                              horizontal_flip_prob=None)
+
+        # BUILD MODEL
+        lighningmodel = LightningModel(model=EFSNet(device=DEVICE,
+                                                    verbose=VERBOSE),
+                                       model_name=MODEL_NAME,
+                                       description=DESCRIPTION,
+                                       ignore_index=IGNORE_INDEX,
+                                       device=DEVICE)
+        # TRAIN
+        trainer = Trainer(model_name=MODEL_NAME,
+                          max_epochs=EPOCHS,
+                          ckpt_dir=os.path.join(ReportingPathDir.get('dir_path'), 'ckpt'),
+                          verbose=VERBOSE)
+        trainer(model=lighningmodel,
+                train_batch_size=BATCH_SIZE,
+                val_batch_size=TEST_SIZE,
+                train_data=tr_td,
+                test_data=val_td,
+                from_checkpoint=True,
+                validate_model=True,
+                test_model=False)
